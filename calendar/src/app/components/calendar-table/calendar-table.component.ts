386dcd4afd4bae342855980b086e367d5aed6fe0
Oksana Kushchik <okushchik@gmail.com>
2020-12-20 12:46:40 +0200

    header of table

--- calendar/src/app/components/calendar-table/calendar-table.component.ts
+++ calendar/src/app/components/calendar-table/calendar-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DateService } from '../../services/date.service';
 
 @Component({
   selector: 'app-calendar-table',
@@ -8,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
 export class CalendarTableComponent implements OnInit {
 
   // private teams: { [key in UserRealm]?: Team } = {};
+  date: Date;
+  daysInMonth: Number;
+  arrOfDays: Array<any>;
 
-  constructor() { }
+  constructor(private _dateService: DateService) {
+    this.date = new Date
+   }
 
   ngOnInit() {
+    
+    this.getDaysInMonth ();
+    this.getArrOfDays ();
+
+    this._dateService.switchMonth().subscribe(
+      (val) => {
+        this.date = val
+        this.getDaysInMonth ();
+        this.getArrOfDays ()
+      })
+      
+
+
     // you need to get users
     // then construct your team by getting users, such as
     /*
@@ -28,7 +47,31 @@ export class CalendarTableComponent implements OnInit {
     * */
     // for now you should be have a teams
   }
+  
+  getDaysInMonth () {
+    this.daysInMonth = new Date(this.date.getFullYear(), this.date.getMonth() + 1, 0).getDate();
+  }
 
+  getArrOfDays (){
+    this.arrOfDays = [];
+    for(let i = 0; i<this.daysInMonth; i++){
+      this.arrOfDays.push({
+        num:i+1,
+        week: new Date(this.date.getFullYear(), this.date.getMonth(), i+1).toString().slice(0, 2),
+        isWeekend: false
+      })
+    }
+    this.getWeekend();
+  }
+
+  getWeekend (){
+    for(let i = 0; i<this.arrOfDays.length; i++){
+      if(this.arrOfDays[i].week == 'Sa' || this.arrOfDays[i].week == 'Su') {
+        this.arrOfDays[i].isWeekend = true
+      }
+    }
+
+  }
   // get teamsEntity(): Team[] {}
 
   // monthDaysEntity(): Day[] {}
